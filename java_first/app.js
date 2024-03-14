1466f76720f071863495fa1e0e161c8609da2c9f
mohanshyamchaudhary <97430097+mohanshyamchaudhary@users.noreply.github.com>
2024-03-14 09:59:40 +0530

    Add files via upload

---
+++ java_first/app.js
@@ -0,0 +1,11 @@
+console.log("hello Monu");
+console.log("welocome to java script");
+let a=20;
+let b=23
+sum=a+b;
+console.log("the sum is :",sum);
+alert("this is alert message");
+console.error("this is a error msg");
+console.warn("this is warn msg");
+let na=prompt("enter your name");
+console.log("how are you ",na);
